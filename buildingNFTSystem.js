45af005a94db993c19f5783206e68d2832ca79b8
BlueSkiesPirate <sebastianreyna128@gmail.com>
2025-07-16 23:37:22 -0400

    layed out case study pages

---
+++ buildingNFTSystem.js
@@ -0,0 +1,52 @@
+const tableOfContents = document.getElementById("table-of-contents");
+
+const contentsLst = [
+  "Needs Assesment",
+  "Materials",
+  "Project overview",
+  "Design + Farm planning document",
+  "(SOP) Standard operating Procedure",
+  "Visuals",
+  "Challenges + Solutions",
+  "Results + Conclusion",
+];
+contentsLst.map((content, i) => {
+  const contentItem = document.createElement("div");
+  contentItem.innerHTML = ` <div class="flex ml-auto mr-auto justify-between items-center w-2/3 "> <div class="w-14 h-14 bg-[#8E518D] text-white flex justify-center items-center">${
+    i + 1
+  }</div><div class="mr-5">${content}</div></div>`;
+  tableOfContents.appendChild(contentItem);
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
--- scrollAnimations.js
+++ scrollAnimations.js
@@ -3,6 +3,11 @@ const prog1 = document.getElementById("progress-1");
 const prog2 = document.getElementById("progress-2");
 const prog3 = document.getElementById("progress-3");
 const prog4 = document.getElementById("progress-4");
+
+const prog_bub1 = document.getElementById("prog-bubble-1");
+const prog_bub2 = document.getElementById("prog-bubble-2");
+const prog_bub3 = document.getElementById("prog-bubble-3");
+
 console.log("connected");
 
 background.addEventListener("scroll", () => {
@@ -10,6 +15,7 @@ background.addEventListener("scroll", () => {
   console.log("Scroll Y: ", scrollY);
   if (scrollY < 500) {
     prog1.style.borderLeftStyle = "dashed";
+    prog_bub1.style.backgroundColor = "transparent";
     prog2.style.borderLeftStyle = "dashed";
     prog3.style.borderLeftStyle = "dashed";
     prog4.style.borderLeftStyle = "dashed";
@@ -17,27 +23,45 @@ background.addEventListener("scroll", () => {
 
   if (scrollY > 500 && scrollY < 1300) {
     prog1.style.borderLeftStyle = "solid";
+    prog_bub1.style.backgroundColor = "#8E518D"; //Secondary background color
 
+    prog_bub2.style.backgroundColor = "transparent"; //Secondary background color
     prog2.style.borderLeftStyle = "dashed";
     prog3.style.borderLeftStyle = "dashed";
     prog4.style.borderLeftStyle = "dashed";
   }
 
-  if (scrollY > 1500 && scrollY < 2000) {
+  if (scrollY > 1500 && scrollY < 1700) {
     prog2.style.borderLeftStyle = "solid";
+    prog_bub2.style.backgroundColor = "#8E518D"; //Secondary background color
+
+    prog_bub3.style.backgroundColor = "transparent"; //Secondary background color
     prog3.style.borderLeftStyle = "dashed";
     prog4.style.borderLeftStyle = "dashed";
   }
 
-  if (scrollY > 3000) {
+  if (scrollY > 1700) {
     prog3.style.borderLeftStyle = "solid";
     prog4.style.borderLeftStyle = "solid";
+    prog_bub3.style.backgroundColor = "#8E518D"; //Secondary background color
   }
-  //   if (scrollY < 2000) {
-  //   prog1.style.borderLeftStyle = "dashed";
-  // }
-
-  // if (scrollY > 500) {
-  //   prog1.style.borderLeftStyle = "solid";
-  // }
 });
+
+//WORD ANIMATIONS
+const word1 = document.getElementById("highLigh(1)");
+const word2 = document.getElementById("highLigh(2)");
+const word3 = document.getElementById("highLigh(3)");
+const word4 = document.getElementById("highLigh(4)");
+
+const lstOfWords = [word1, word2, word3, word4];
+let indexOfWord = 0;
+
+setInterval(() => {
+  for (let i = 0; i < lstOfWords.length; i++) {
+    if (i != indexOfWord) {
+      lstOfWords[i].style.color = "black";
+    }
+  }
+  lstOfWords[indexOfWord].style.color = "#8E518D";
+  indexOfWord = (indexOfWord + 1) % lstOfWords.length;
+}, 1000); // Executes every 1 second
